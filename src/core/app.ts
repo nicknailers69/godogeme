0a9e40f1b469aaae6771053dc633eb1e370c8ede
wizegene <ncloutier@wizegene.com>
2021-04-01 18:07:21 -0400

    app start

---
+++ src/core/app.ts
@@ -0,0 +1,22 @@
+import Express from "express";
+import Cors from "cors";
+import Helmet from "helmet";
+import Routes from "../config/routes";
+
+class App  {
+
+    protected app:Express.Application;
+    protected router:Express.Router;
+
+    constructor() {
+        this.app = Express();
+
+    }
+
+    init() {
+        this.app.use(Cors());
+        this.app.use(Helmet());
+        this.app.use(Routes(this.router))
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/*
+    GoDogeMe Backend
+    version: 1.0.0
+    author: Nicolas Cloutier <nicknailers69@gmail.com>
+
+ */
